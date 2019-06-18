fec0e6a20f9e6ea81b87e147efc48a0daac3b885
Jon Antolin <jonantolin@gmail.com>
2019-06-18 14:01:44 +0200

    Cambios en aula

--- src/com/ipartek/formacion/Aula.java
+++ src/com/ipartek/formacion/Aula.java
@@ -1,8 +1,8 @@
 package com.ipartek.formacion;
 
 import java.util.ArrayList;
-
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class Aula {
 	
@@ -51,6 +51,34 @@ public class Aula {
 		return mensaje;
 	}
 	
-
+public static void main(String[] args) {
+	
+	Scanner sc = new Scanner(System.in);
+	int opcion = 0;
+	
+	while(opcion != 0) {
+		System.out.println("Introduzca la opción que quiera: "
+				+ "\nListar alumnos"
+				+ "\nCrear alumno"
+				+ "\nEliminar alumno"
+				+ "\nBuscar Alumno");
+		opcion = sc.nextInt();
+		switch(opcion) {
+		
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		default:
+			System.out.println("Introduce una opción metiendo un número del 1 al 4 o 0 para salir");
+		}
+	}
+	
+	sc.close();
+}
 
 }
