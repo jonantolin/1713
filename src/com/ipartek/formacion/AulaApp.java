3320ae509af2237a40737956d870b2b6ef13bffb
Jon <Jon@Jon-PC>
2019-06-18 20:11:10 +0200

    Version 1.0
    TODO Documentar, detalles

--- src/com/ipartek/formacion/AulaApp.java
+++ src/com/ipartek/formacion/AulaApp.java
@@ -12,9 +12,12 @@ public class AulaApp {
 		Aula aula = new Aula();
 		aula.rellenarLista();
 		
-		int opcion = 0;
+		String opcion ="0";
 		
 		do{
+			// opcion=6;
+			
+			
 			System.out.println("\nIntroduzca la opción que quiera (introduzca número): "
 					+ "\n1 - Listar alumnos"
 					+ "\n2 - Crear alumno"
@@ -22,39 +25,51 @@ public class AulaApp {
 					+ "\n4 - Buscar Alumno Afortunado para leer"
 					+ "\n0- Salir"
 					+ "\nIntroduzca numero: ");
-			opcion = sc.nextInt();
+				
+				
+				opcion = sc.next();
+				
 			switch(opcion) {
 			
-				case 1:
+				case "1":
 					aula.listado();
 					break;
-				case 2: 
+					
+				case "2": 
 					System.out.println("\nIntroduzca nombre del nuevo alumno: ");
 					String nom =""; 
-					Scanner sc3 = new Scanner(System.in);
-					nom = (String)sc3.nextLine();
+					//Scanner sc3 = new Scanner(System.in);
+					sc = new Scanner(System.in);
+					nom = (String)sc.nextLine();
 					aula.nuevoAlumno(nom);
 					System.out.println("Introducido con éxito\n");
 					break;
-				case 3:
+					
+				case "3":
 					System.out.println("\nIntroduzca nombre que desea ELIMINAR");
 					
-					Scanner sc2 = new Scanner(System.in);
-					String nombre = sc2.nextLine(); 
+					 sc = new Scanner(System.in);
+					String nombre = (String)sc.nextLine(); 
 					//nombre = sc2.next();
 					
 					aula.eliminarAlumno(nombre);
 					break;
-				case 4:
+					
+				case "4":
 					System.out.println(aula.afortunado());
 					break;
-				case 0: 
+					
+				case "0": 
 					System.out.println("Hasta pronto");
 					break;
+					
 				default:
 					System.out.println("Introduce una opción metiendo un número del 1 al 4 o 0 para salir");
-			}		
-		}while(opcion != 0);
+			}
+			
+			
+			
+		}while(!opcion.equals("0"));
 		
 		sc.close();
 	}
