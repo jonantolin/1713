05e7f76e57f6ac7143b4a9be39349be3a2b2087c
Jon <Jon@Jon-PC>
2019-06-19 23:17:07 +0200

    TODO Revisar

--- src/com/ipartek/formacion/CerdosYDiamantes.java
+++ src/com/ipartek/formacion/CerdosYDiamantes.java
@@ -1,30 +1,42 @@
 package com.ipartek.formacion;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 import org.junit.Test;
 
-public class CerdosYDiamantes implements ComparadorCerdosDiamantes {
+public class CerdosYDiamantes  {
 
 	
 	@Test
 	public void test() {
 
-		ArrayList lista = new ArrayList();
-		Cerdo c = new Cerdo();
-		Diamante d = new Diamante();
+		ArrayList<Object> lista = new ArrayList<Object>();
+		Cerdo c = new Cerdo(3);
+		Diamante d = new Diamante(1);
+		Cerdo c2 = new Cerdo(30);
+		Diamante d2 = new Diamante(40);
+		Cerdo c3 = new Cerdo(5);
+		Diamante d3 = new Diamante(10);
 		
 		lista.add(c);
 		lista.add(d);
+		lista.add(c2);
+		lista.add(d2);
+		lista.add(c3);
+		lista.add(d3);
+		
+		Collections.sort(lista, new ComparadorCerdosDiamantes());
+		
+		assertEquals(1, lista.get(0));
+		
+		//Collections.sort(lista, new ComparadorCerdosDiamantes());
 		
 	}
 
-	@Override
-	public int comparar(Object a, Object b) {
-	//	if(a instanceof Cerdo) {
-	//		Cerdo a = (Cerdo) a;
-	//	}
-		return 0;
-	}
+	
+	
 
 }
--- src/com/ipartek/formacion/ComparadorCerdosDiamantes.java
+++ src/com/ipartek/formacion/ComparadorCerdosDiamantes.java
@@ -1,7 +1,38 @@
 package com.ipartek.formacion;
 
-public interface ComparadorCerdosDiamantes {
+import java.util.Comparator;
 
-	public int comparar(Object a, Object b); // Cerdos y Diamantes/
+public class ComparadorCerdosDiamantes implements Comparator<Object> {
+
+	
+	
+	@Override
+	public int compare(Object a, Object b) {
+		
+		int anterior=0;
+		int posterior=0;
+		
+		if(a instanceof Cerdo) {
+			Cerdo c1 = (Cerdo) a;	
+			anterior= c1.getValor();
+			
+		}else if(a instanceof Diamante) {
+			Diamante d1 = (Diamante) a;
+			anterior= d1.getValor();
+		}
+		
+		if(b instanceof Cerdo) {
+			Cerdo c2 = (Cerdo) b;
+			posterior = c2.getValor();
+		}
+		else if(b instanceof Diamante) {
+			Diamante d2 = (Diamante) b;
+			posterior = d2.getValor();
+		}
+		return anterior - posterior;
+	
+	} // Cerdos y Diamantes/
+
+
 	
 }
